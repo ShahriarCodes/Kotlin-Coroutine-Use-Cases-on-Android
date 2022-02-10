23d3472cb661560e07b0816dbb5bb32a0ea0dc0c
shahriarcodes <shahriar.hasan100@gmail.com>
2022-02-10 20:08:12 +0600

    usecase 6: RetryNetworkRequestViewModel exponential backoff to retry

--- app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/usecases/coroutines/usecase6/RetryNetworkRequestViewModel.kt
+++ app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/usecases/coroutines/usecase6/RetryNetworkRequestViewModel.kt
@@ -4,6 +4,7 @@ import androidx.lifecycle.viewModelScope
 import com.lukaslechner.coroutineusecasesonandroid.base.BaseViewModel
 import com.lukaslechner.coroutineusecasesonandroid.mock.MockApi
 import kotlinx.coroutines.TimeoutCancellationException
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withTimeout
 import timber.log.Timber
@@ -26,6 +27,29 @@ class RetryNetworkRequestViewModel(
 
         }
     }
+
+    private suspend fun <T> retryExponentialBackoff(
+        numberOfRetries: Int,
+        initialDelayMillis: Long = 100,
+        maxDelayMillis: Long = 1000,
+        factor: Double = 2.0,
+        block: suspend () -> T
+    ): T {
+        var currentDelay = initialDelayMillis
+        repeat(numberOfRetries) {
+            try {
+                return block()
+
+            } catch (e: Exception) {
+                Timber.e(e)
+            }
+            delay(currentDelay)
+            currentDelay = (currentDelay * factor).toLong().coerceAtMost(maxDelayMillis)
+        }
+
+        return block()
+    }
+
 
     private suspend fun <T> retry(numberOfRetries: Int, block: suspend () -> T): T {
         repeat(numberOfRetries) {
