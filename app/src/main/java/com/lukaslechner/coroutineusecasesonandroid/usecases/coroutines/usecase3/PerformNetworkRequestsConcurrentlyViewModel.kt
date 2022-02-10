6d3d221e41e36e638df0e39091413e8ae788699b
shahriarcodes <shahriar.hasan100@gmail.com>
2022-02-10 07:03:23 +0600

    usecase 3: performNetworkRequestsConcurrently different approach

--- app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/usecases/coroutines/usecase3/PerformNetworkRequestsConcurrentlyViewModel.kt
+++ app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/usecases/coroutines/usecase3/PerformNetworkRequestsConcurrentlyViewModel.kt
@@ -4,6 +4,7 @@ import androidx.lifecycle.viewModelScope
 import com.lukaslechner.coroutineusecasesonandroid.base.BaseViewModel
 import com.lukaslechner.coroutineusecasesonandroid.mock.MockApi
 import kotlinx.coroutines.async
+import kotlinx.coroutines.awaitAll
 import kotlinx.coroutines.joinAll
 import kotlinx.coroutines.launch
 
@@ -28,6 +29,35 @@ class PerformNetworkRequestsConcurrentlyViewModel(
     }
 
     fun performNetworkRequestsConcurrently() {
+        uiState.value = UiState.Loading
+
+        val oreoFeatures = viewModelScope.async { mockApi.getAndroidVersionFeatures(27) }
+        val pieFeatures = viewModelScope.async { mockApi.getAndroidVersionFeatures(28) }
+        val android10Features = viewModelScope.async { mockApi.getAndroidVersionFeatures(29) }
+
+        viewModelScope.launch {
+            try {
+//                val versionFeatures = listOf(
+//                    oreoFeatures.await(), // coroutine already running.. await doesn't initialize them
+//                    pieFeatures.await(),
+//                    android10Features.await()
+//                )
+
+                val versionFeatures = awaitAll(
+                    oreoFeatures,
+                    pieFeatures,
+                    android10Features
+                )
+
+                uiState.value = UiState.Success(versionFeatures)
+            } catch (exception: Exception) {
+                uiState.value = UiState.Error("Network request failed!")
+            }
+        }
+    }
+
+
+    fun performNetworkRequestsConcurrently2() {
         uiState.value = UiState.Loading
         viewModelScope.launch {
             try {
